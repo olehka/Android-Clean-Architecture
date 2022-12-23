e6b360dae7651f9722b45066a6d710f0126cbbac
Oleh Kapustianov <olehka.dev@gmail.com>
2022-12-23 12:24:02 +0100

    Create a new shared module

---
+++ shared/src/commonMain/kotlin/com/olehka/cleanandroid/shared/Greeting.kt
@@ -0,0 +1,9 @@
+package com.olehka.cleanandroid.shared
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
